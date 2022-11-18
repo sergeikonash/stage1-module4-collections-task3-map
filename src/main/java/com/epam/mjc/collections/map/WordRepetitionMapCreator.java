38bf8c2e9f7ac1dc7117f106127380794b9c090a
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-18 11:45:30 +0100

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        sentence = sentence.toLowerCase();
+        sentence = sentence.replaceAll(",", "");
+        sentence = sentence.replaceAll("\\.", "");
+        List<String> list = Arrays.asList(sentence.split(" "));
+        for (String s : list) {
+            s = s.trim();
+            if (!map.containsKey(s)) {
+                map.put(s, 1);
+            } else {
+                map.put(s, map.get(s) + 1);
+            }
+        }
+        return map;
     }
 }
